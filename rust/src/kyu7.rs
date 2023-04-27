ce32605adfee5f83e5d54869545337784e63352e
Sergey Zubkov <ksevelyar@protonmail.com>
2023-04-27 09:05:23 +0300

    solve rust/7kyu/reverse_words

--- rust/src/kyu7.rs
+++ rust/src/kyu7.rs
@@ -1,3 +1,4 @@
 pub mod exes_and_ohs;
 pub mod remove_smallest;
+pub mod reverse_words;
 pub mod sum_of_odd_numbers;
---
+++ rust/src/kyu7/reverse_words.rs
@@ -0,0 +1,28 @@
+pub fn reverse_words(str: &str) -> String {
+    str.split_inclusive(' ').map(reverse_word).collect()
+}
+
+fn reverse_word(word: &str) -> String {
+    word.chars().fold(String::new(), |mut acc, char| {
+        match char.is_whitespace() {
+            true => acc.push(char),
+            false => acc.insert(0, char),
+        }
+
+        acc
+    })
+}
+
+#[test]
+fn sample_test() {
+    assert_eq!(
+        reverse_words("double   spaced   words"),
+        "elbuod   decaps   sdrow"
+    );
+    assert_eq!(
+        reverse_words("The quick brown fox jumps over the lazy dog."),
+        "ehT kciuq nworb xof spmuj revo eht yzal .god"
+    );
+    assert_eq!(reverse_words("apple"), "elppa");
+    assert_eq!(reverse_words("a b c d"), "a b c d");
+}
