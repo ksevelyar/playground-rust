b9b30ea593486d7866d50c404d08884e36ae9413
Sergey Zubkov <ksevelyar@protonmail.com>
2024-07-22 21:09:28 +0300

    100 exercises: 5.9

--- 100-exercises-to-learn-rust/exercises/05_ticket_v2/07_unwrap/src/lib.rs
+++ 100-exercises-to-learn-rust/exercises/05_ticket_v2/07_unwrap/src/lib.rs
@@ -2,7 +2,13 @@
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    match Ticket::new(title.clone(), description, status.clone()) {
+        Err(err) if err.starts_with("Description") => {
+            easy_ticket(title, "Description not provided".into(), status)
+        }
+        Err(err) => panic!("{err}"),
+        Ok(ticket) => ticket,
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
--- 100-exercises-to-learn-rust/exercises/05_ticket_v2/09_error_trait/src/lib.rs
+++ 100-exercises-to-learn-rust/exercises/05_ticket_v2/09_error_trait/src/lib.rs
@@ -2,18 +2,39 @@
 //  When implementing `Display`, you may want to use the `write!` macro from Rust's standard library.
 //  The docs for the `std::fmt` module are a good place to start and look for examples:
 //  https://doc.rust-lang.org/std/fmt/index.html#write
+use std::error::Error;
+use std::fmt;
 
+#[derive(Debug)]
 enum TicketNewError {
     TitleError(String),
     DescriptionError(String),
 }
+
+impl fmt::Display for TicketNewError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let error = match &self {
+            TicketNewError::DescriptionError(error) => error,
+            TicketNewError::TitleError(error) => error,
+        };
+        write!(f, "{}", error)
+    }
+}
+
+impl Error for TicketNewError {}
 
 // TODO: `easy_ticket` should panic when the title is invalid, using the error message
 //   stored inside the relevant variant of the `TicketNewError` enum.
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    match Ticket::new(title.clone(), description, status.clone()) {
+        Err(TicketNewError::DescriptionError(_error)) => {
+            easy_ticket(title, "Description not provided".into(), status)
+        }
+        Err(TicketNewError::TitleError(error)) => panic!("{error}"),
+        Ok(ticket) => ticket,
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
