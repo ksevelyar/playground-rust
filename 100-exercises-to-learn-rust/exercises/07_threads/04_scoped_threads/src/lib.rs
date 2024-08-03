e01eaec3ab21ff21defe55059bb370d3f17d0372
Sergey Zubkov <ksevelyar@protonmail.com>
2024-08-03 23:45:59 +0300

    100 exercises: 7.4

--- 100-exercises-to-learn-rust/exercises/07_threads/04_scoped_threads/src/lib.rs
+++ 100-exercises-to-learn-rust/exercises/07_threads/04_scoped_threads/src/lib.rs
@@ -3,7 +3,15 @@
 //  Don't perform any heap allocation. Don't leak any memory.
 
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+    let mid = v.len() / 2;
+
+    std::thread::scope(|scope| {
+        let first = scope.spawn(|| v[..mid].iter().sum::<i32>());
+
+        let second = scope.spawn(|| v[mid..].iter().sum::<i32>());
+
+        first.join().unwrap() + second.join().unwrap()
+    })
 }
 
 #[cfg(test)]
