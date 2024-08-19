8a20707a0075bf7ad21ad6a8629ae39412102fc7
Sergey Zubkov <ksevelyar@protonmail.com>
2024-08-19 21:11:19 +0300

    rustlings: 12.1

--- rustlings/exercises/12_options/options1.rs
+++ rustlings/exercises/12_options/options1.rs
@@ -3,7 +3,11 @@
 // someone eats it all, so no icecream is left (value 0). Return `None` if
 // `hour_of_day` is higher than 23.
 fn maybe_icecream(hour_of_day: u16) -> Option<u16> {
-    // TODO: Complete the function body.
+    match hour_of_day {
+        0..22 => Some(5),
+        22..24 => Some(0),
+        _ => None,
+    }
 }
 
 fn main() {
@@ -19,7 +23,7 @@ mod tests {
         // TODO: Fix this test. How do you get the value contained in the
         // Option?
         let icecreams = maybe_icecream(12);
-        assert_eq!(icecreams, 5);
+        assert_eq!(icecreams.unwrap(), 5);
     }
 
     #[test]
