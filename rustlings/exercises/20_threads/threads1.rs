8556b1b2f7ad2c5c4f8a444f1088f5c02e7ca5d4
Sergey Zubkov <ksevelyar@protonmail.com>
2024-08-25 22:03:04 +0300

    rustlings: 20.3

--- rustlings/exercises/20_threads/threads1.rs
+++ rustlings/exercises/20_threads/threads1.rs
@@ -20,11 +20,7 @@ fn main() {
         handles.push(handle);
     }
 
-    let mut results = Vec::new();
-    for handle in handles {
-        // TODO: Collect the results of all threads into the `results` vector.
-        // Use the `JoinHandle` struct which is returned by `thread::spawn`.
-    }
+    let results = handles.into_iter().map(|handle| handle.join().unwrap());
 
     if results.len() != 10 {
         panic!("Oh no! Some thread isn't done yet!");
