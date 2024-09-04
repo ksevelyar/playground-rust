e6b14d2bfa6d049978c704f263a3b032f9e3c28f
Sergey Zubkov <ksevelyar@protonmail.com>
2024-09-04 22:52:33 +0300

    discovery: 3.4

--- discovery/flake.nix
+++ discovery/flake.nix
@@ -4,24 +4,27 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
-      system:
-      let
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachSystem ["x86_64-linux"] (
+      system: let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ rust-overlay.overlays.default ];
+          overlays = [rust-overlay.overlays.default];
         };
-      in
-      {
-        devShell = pkgs.mkShell
+      in {
+        devShell =
+          pkgs.mkShell
           {
             nativeBuildInputs = with pkgs; [
               (
                 rust-bin.stable.latest.default.override {
-                  targets = [
-                    "thumbv7em-none-eabihf"
-                  ];
+                  targets = ["thumbv7em-none-eabihf"];
+                  extensions = ["llvm-tools-preview"];
                 }
               )
               gdb
