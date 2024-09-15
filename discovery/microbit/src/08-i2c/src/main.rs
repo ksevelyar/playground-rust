9b8fb4c1104afbe6e5f58359555e843abc59ca14
Sergey Zubkov <ksevelyar@protonmail.com>
2024-09-15 21:34:52 +0300

    discovery: 8.5

--- discovery/microbit/src/08-i2c/src/main.rs
+++ discovery/microbit/src/08-i2c/src/main.rs
@@ -1,10 +1,11 @@
-#![deny(unsafe_code)]
+//#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
+use core::fmt::Write;
 use cortex_m_rt::entry;
-use panic_rtt_target as _;
 use heapless::Vec;
+use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
 use microbit::{
@@ -15,7 +16,7 @@ use microbit::{
 
 use microbit::{hal::twim, pac::twim0::frequency::FREQUENCY_A};
 
-use lsm303agr::{AccelOutputDataRate, Lsm303agr};
+use lsm303agr::{AccelOutputDataRate, Lsm303agr, MagOutputDataRate};
 
 mod serial_setup;
 use serial_setup::UartePort;
@@ -31,6 +32,7 @@ fn main() -> ! {
     let mut sensor = Lsm303agr::new_with_i2c(i2c);
     sensor.init().unwrap();
     sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
+    sensor.set_mag_odr(MagOutputDataRate::Hz50).unwrap();
 
     let mut serial = {
         let serial = uarte::Uarte::new(
@@ -44,10 +46,46 @@ fn main() -> ! {
     let mut buffer: Vec<u8, 32> = Vec::new();
 
     loop {
-        if sensor.accel_status().unwrap().xyz_new_data {
-            let data = sensor.accel_data().unwrap();
-            // RTT instead of normal print
-            rprintln!("Acceleration: x {} y {} z {}", data.x, data.y, data.z);
+        let byte = nb::block!(serial.read()).unwrap();
+
+        match byte {
+            13 => {
+                match core::str::from_utf8(&buffer) {
+                    Ok("magnetometer") => {
+                        if sensor.mag_status().unwrap().xyz_new_data {
+                            let data = sensor.mag_data().unwrap();
+                            write!(
+                                serial,
+                                "magnetometer: x {} y {} z {}\r\n",
+                                data.x, data.y, data.z
+                            )
+                            .unwrap()
+                        }
+                    }
+                    Ok("accelerometer") => {
+                        if sensor.accel_status().unwrap().xyz_new_data {
+                            let data = sensor.accel_data().unwrap();
+                            write!(
+                                serial,
+                                "accelerometer: x {} y {} z {}\r\n",
+                                data.x, data.y, data.z
+                            )
+                            .unwrap()
+                        }
+                    }
+                    Err(err) => write!(serial, "{}", err).unwrap(),
+                    _ => (),
+                }
+                buffer.clear();
+            }
+            _ => {
+                if buffer.push(byte).is_err() {
+                    write!(serial, "error: buffer full\r\n").unwrap();
+                    buffer.clear();
+                }
+            }
         }
+
+        nb::block!(serial.flush()).unwrap()
     }
 }
