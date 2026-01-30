82b66c2af1356d9a9302a4fafcd8c8e692a9c51a
Sergey Zubkov <ksevelyar@protonmail.com>
2026-01-30 23:46:29 +0300

    clean up

--- bevy-2d/flake.nix
+++ bevy-2d/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/b58df7fc9d5f02c269091f2b0b81a6e06fc859bb";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
